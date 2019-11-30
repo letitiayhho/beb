8d382a99a19f0ab496adf36e0c037613cb857a21
Letitia Ho <letitiayhho@gmail.com>
2019-11-30 19:32:12 +0000

    No longer using ElemTree since raw corpus isn't reaaally in XML

--- hkcancor_preprocess.py
+++ hkcancor_preprocess.py
@@ -1,7 +1,8 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 import os
 import pandas as pd
 from random import random
+from hanzidentifier import has_chinese
 
 sentences = []
 labels = []
@@ -10,8 +11,39 @@ hkcancor_path = '../corpora/hkcancor/raw'
 w_hkcancorTSV = '../corpora/hkcancor/hkcancor.tsv'
 
 for filename in os.listdir(hkcancor_path):
+    file = open(filename)
+    sentence = []
+
+    while True:
+        line = file.readline()
+        if "</sent_tag>" in line:
+            sentences.append(sentence)
+            sentence = []
+        if has_chinese(line):
+            sentence.append
+        for char in line:
+            if has_chinese(char):
+                sentence.append(char)
+        
+
+
+
+    while True:
+        line = file.readline()
+        if "<sent_tag>" in line:
+            x = True
+            sentence = []
+        while x:
+            if has_chinese(char)
+            sentence.append(
+            
+
+            if "</sent_tag>" in line:
+                x = False
+
+    
     with open(os.path.join(hkcancor_path, filename)) as fp:
-        tree = ElementTree.parse(fp)
+        tree = ET.parse(fp)
         root = tree.getroot()
         for sentence in root.iter('sent_tag'):
             sentences.append(sentence)
