a045fecf3eaf225f426684f98daebef495d068d6
Letitia Ho <letitiayhho@gmail.com>
2019-11-30 20:22:07 +0000

    Debugging

--- hkcancor_preprocess.py
+++ hkcancor_preprocess.py
@@ -11,46 +11,25 @@ hkcancor_path = '../corpora/hkcancor/raw'
 w_hkcancorTSV = '../corpora/hkcancor/hkcancor.tsv'
 
 for filename in os.listdir(hkcancor_path):
-    file = open(filename)
+    file = open(os.path.join(hkcancor_path, filename))
+    print("processing: " + filename)
     sentence = []
 
     while True:
         line = file.readline()
-        if "</sent_tag>" in line:
-            sentences.append(sentence)
-            sentence = []
         if has_chinese(line):
-            sentence.append
-        for char in line:
-            if has_chinese(char):
-                sentence.append(char)
-        
-
-
-
-    while True:
-        line = file.readline()
-        if "<sent_tag>" in line:
-            x = True
-            sentence = []
-        while x:
-            if has_chinese(char)
-            sentence.append(
-            
-
-            if "</sent_tag>" in line:
-                x = False
-
-    
-    with open(os.path.join(hkcancor_path, filename)) as fp:
-        tree = ET.parse(fp)
-        root = tree.getroot()
-        for sentence in root.iter('sent_tag'):
-            sentences.append(sentence)
+            char = line.split('/', 1)[0]
+            sentence.append(char[3:])
+        elif char == "。" or char == "，":
+            sentence.append(char)
+        if "</sent_tag>" in line:
             labels.append(0) if random() <= 0.3 else labels.append(1)
+            print(''.join(sentence))
+            sentences.append(''.join(sentence))
+            sentence = []
 
-for i in range(20):
-    print(label[i] + '\t' + sentences[i])
+#for i in range(20):
+#    print(labels[i] + '\t' + sentences[i])
 
 #df = {'labels': labels, 'sentences': sentences}
 
