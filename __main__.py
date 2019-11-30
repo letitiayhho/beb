24f2f2c14740a2e0569a8935a25bf184da9bd9a4
Kevin Murphy <kmurphy4@swarthmore.edu>
2019-11-30 12:23:45 -0500

    Add command line args for fine-tuning the model.

--- __main__.py
+++ __main__.py
@@ -1,12 +1,21 @@
 import argparse
 
-from .tokenization import tokenize
+from .fine_tune_model import fine_tune
+#from .tokenization import tokenize
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('corpus_name')
+    parser.add_argument('train_corpus')
+    parser.add_argument('dev_corpus')
+    parser.add_argument('--columns', nargs='*')
     args = parser.parse_args()
 
     print(args)
 
+    fine_tune(args.corpus_name,
+              args.train_corpus,
+              args.dev_corpus,
+              args.column_names)
     #tokenize(args.bagus)
