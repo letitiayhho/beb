e8c23b509852944c1d744b1e17d005527f8551dc
Letitia Ho <letitiayhho@gmail.com>
2019-11-30 20:45:21 +0000

    More debugging, added another internal "while" loop to process characters inside <sent_tag>, gave up and used a "for" loop to check each character in each line (within the <sent_tag> tags) for Chinese characters and punctuation

--- hkcancor_preprocess.py
+++ hkcancor_preprocess.py
@@ -14,19 +14,35 @@ for filename in os.listdir(hkcancor_path):
     file = open(os.path.join(hkcancor_path, filename))
     print("processing: " + filename)
     sentence = []
+    x = True
 
     while True:
         line = file.readline()
-        if has_chinese(line):
-            char = line.split('/', 1)[0]
-            sentence.append(char[3:])
-        elif char == "。" or char == "，":
-            sentence.append(char)
-        if "</sent_tag>" in line:
-            labels.append(0) if random() <= 0.3 else labels.append(1)
-            print(''.join(sentence))
-            sentences.append(''.join(sentence))
-            sentence = []
+
+        if "<sent_tag>" in line:
+            x = True
+
+        while x:
+            for char in line:
+                if has_chinese(char):
+                    sentence.append(char)
+                elif char == "。" or char == "，":
+                    sentence.append(char)
+
+            if "</sent_tag>" in line:
+                labels.append(0) if random() <= 0.3 else labels.append(1)
+                print(''.join(sentence))
+                sentences.append(''.join(sentence))
+                sentence = []
+                x = False
+                break
+
+            break
+
+        if not line:
+            break
+
+    file.close()
 
 #for i in range(20):
 #    print(labels[i] + '\t' + sentences[i])
